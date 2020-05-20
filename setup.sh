58a558bde60bb544674d397a9547d81326ace816
Aayushman Choudhary <Aayushmanchaudhory@gmail.com>
2020-05-20 21:54:56 +0530

    Cleanup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]
+then echo "Please run the script as root :)"
+  exit
+fi
 cd /usr/local/bin/
-#git clone ''
-cd copysearch
+git clone 'https://github.com/crowded-geek/csearch'
+cd csearch
 pip install pyperclip
-bind '"\C-m"':'"/usr/bin/python3 /usr/local/bin/copysearch/copysearch.py\n"'
+cd
