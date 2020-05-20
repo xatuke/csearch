25e8767d9f8cce168b442800be9cd16d7bd8b48e
Aayushman Choudhary <Aayushmanchaudhory@gmail.com>
2020-05-20 23:15:53 +0530

    Added DuckDuckGo

--- copysearch.py
+++ copysearch.py
@@ -1,4 +1,9 @@
 import webbrowser
 import pyperclip
+import sys
 query = pyperclip.paste();
-webbrowser.open('https://www.google.com/search?&q='+query)
+if(len(sys.argv)>1):
+    if(str(sys.argv[1])=="ddg"):
+        webbrowser.open("https://duckduckgo.com/?q="+query)
+else:
+    webbrowser.open("https://google.com/search?q="+query)
